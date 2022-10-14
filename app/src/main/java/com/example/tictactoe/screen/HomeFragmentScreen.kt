6751071dadc3806402601d5acf0b327651201dc9
MUradyan <hayk.muradyan@zangi.com>
2022-10-14 16:44:24 +0400

    (HM) is modified something

--- app/src/main/java/com/example/tictactoe/screen/HomeFragmentScreen.kt
+++ app/src/main/java/com/example/tictactoe/screen/HomeFragmentScreen.kt
@@ -2,9 +2,12 @@ package com.example.tictactoe.screen
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.Gravity
 import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.RelativeLayout
+import androidx.core.content.ContextCompat
+import com.example.tictactoe.R
 
 class HomeFragmentScreen @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
@@ -13,14 +16,18 @@ class HomeFragmentScreen @JvmOverloads constructor(
     var playWithYourSelfButton: Button? = null
 
     init {
+        setBackgroundColor(ContextCompat.getColor(context, R.color.screen_color))
+        val params = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
+        layoutParams = params
         createPlayWithYourSelfButton()
     }
 
     private fun createPlayWithYourSelfButton() {
         playWithYourSelfButton = Button(context)
         val params = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+        params.gravity = Gravity.CENTER
         playWithYourSelfButton?.layoutParams = params
-        playWithYourSelfButton?.text = "Play"
+        playWithYourSelfButton?.text = context.getString(R.string.play_text)
         addView(playWithYourSelfButton)
     }
 }
